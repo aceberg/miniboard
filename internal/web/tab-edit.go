b38a723e25cac6d8d0075790fffdd2304ce1d7bd
aceberg <1502200+aceberg@users.noreply.github.com>
2023-07-04 16:58:13 +0700

    Refactor tab-edit.go

--- internal/web/tab-edit.go
+++ internal/web/tab-edit.go
@@ -6,7 +6,6 @@ import (
 	"strconv"
 
 	"github.com/aceberg/miniboard/internal/models"
-	"github.com/aceberg/miniboard/internal/yaml"
 )
 
 func tabEditHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,52 +13,49 @@ func tabEditHandler(w http.ResponseWriter, r *http.Request) {
 
 	guiData.Config = AppConfig
 	guiData.CurrentTab = "Edit Tab"
-	guiData.Links = AllLinks
 
 	tabStr := r.FormValue("tab")
+	action := r.FormValue("action")
 
-	if tabStr != "" {
-		tab, _ := strconv.Atoi(tabStr)
-		guiData.TabEdit = tab
+	tab, _ := strconv.Atoi(tabStr)
+	guiData.TabEdit = tab
 
-		action := r.FormValue("action")
+	if action == "addpan" { // Add panels
+		panels := r.PostForm["panels"]
 
-		if action != "" {
-			if action == "addpan" { // Add panels
-				panels := r.PostForm["panels"]
-
-				i := len(AllLinks.Tabs[tab].Panels)
-				for _, panelName := range panels {
-					AllLinks.Tabs[tab].Panels[i] = panelName
-					i = i + 1
-				}
-				assignPanelIDs(tab)
-
-			} else if action == "deltab" { // Delete Tab
-
-				delete(AllLinks.Tabs, tab)
-				assignTabIDs() // assign-IDs.go
-
-				http.Redirect(w, r, "/tabs/", 302)
-			} else {
-				panStr := r.FormValue("pan")
-				pan, _ := strconv.Atoi(panStr)
-
-				if action == "up" { // Move panel up
-					panelName := AllLinks.Tabs[tab].Panels[pan]
-					AllLinks.Tabs[tab].Panels[pan] = AllLinks.Tabs[tab].Panels[pan-1]
-					AllLinks.Tabs[tab].Panels[pan-1] = panelName
-
-				} else if action == "delpan" { // Delete Panel from tab
-
-					delete(AllLinks.Tabs[tab].Panels, pan)
-					assignPanelIDs(tab) // assign-IDs.go
-				}
-			}
-
-			yaml.Write(AppConfig.YamlPath, AllLinks)
+		i := len(AllLinks.Tabs[tab].Panels)
+		for _, panelName := range panels {
+			AllLinks.Tabs[tab].Panels[i] = panelName
+			i = i + 1
 		}
 	}
+
+	if action == "deltab" { // Delete Tab
+		delete(AllLinks.Tabs, tab)
+		assignAllIDs() // assign-IDs.go
+
+		http.Redirect(w, r, "/tabs/", 302)
+	}
+
+	panStr := r.FormValue("pan")
+	pan, _ := strconv.Atoi(panStr)
+
+	if action == "up" { // Move panel up
+		panelName := AllLinks.Tabs[tab].Panels[pan]
+		AllLinks.Tabs[tab].Panels[pan] = AllLinks.Tabs[tab].Panels[pan-1]
+		AllLinks.Tabs[tab].Panels[pan-1] = panelName
+	}
+
+	if action == "delpan" { // Delete Panel from tab
+		delete(AllLinks.Tabs[tab].Panels, pan)
+	}
+
+	if action != "" { // If changes assighn IDs and write to yaml
+		assignAllIDs() // assign-IDs.go
+		http.Redirect(w, r, "/tab_edit/?tab="+tabStr, 302)
+	}
+
+	guiData.Links = AllLinks
 
 	execTemplate(w, "tab-edit", guiData)
 }
